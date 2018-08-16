6863a6b70b8f57d5fc3db3c143fe806aa193d301
Saikumar <saikv@thoughtworks.com>
2018-08-16 19:19:22 +0530

    Saikumar, Raju | #103 | CheckIn current patients scheduled appointments for starting visit

--- api/src/main/java/org/bahmni/module/bahmnimaadi/OpenmrsVisitAdvice.java
+++ api/src/main/java/org/bahmni/module/bahmnimaadi/OpenmrsVisitAdvice.java
@@ -1,20 +1,42 @@
 package org.bahmni.module.bahmnimaadi;
 
+import org.openmrs.Visit;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appointments.model.Appointment;
 import org.openmrs.module.appointments.service.AppointmentsService;
 import org.springframework.aop.AfterReturningAdvice;
 
 import java.lang.reflect.Method;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.bahmni.module.bahmnimaadi.util.DateUtil.startOfDay;
+import static org.openmrs.module.appointments.model.AppointmentStatus.CheckedIn;
+import static org.openmrs.module.appointments.model.AppointmentStatus.Scheduled;
 
 public class OpenmrsVisitAdvice implements AfterReturningAdvice {
 
+    private static final String SAVE_VISIT_METHOD_NAME = "saveVisit";
+
     public void afterReturning(Object returnValue, Method method, Object[] parameters, Object o1) {
+        if (!SAVE_VISIT_METHOD_NAME.equals(method.getName())) {
+            return;
+        }
         AppointmentsService appointmentsService = Context.getService(AppointmentsService.class);
         List<Appointment> allAppointments = appointmentsService.getAllAppointments(startOfDay());
+        String patientUuid = ((Visit) parameters[0]).getPatient().getUuid();
+        List<Appointment> patientAppointments = getScheduledPatientAppointments(allAppointments, patientUuid);
+        Date checkInDate = new Date();
+        patientAppointments.forEach(appointment ->
+                appointmentsService.changeStatus(appointment, CheckedIn.toString(), checkInDate));
+    }
+
+    private List<Appointment> getScheduledPatientAppointments(List<Appointment> allAppointments, String patientUuid) {
+        return allAppointments.stream()
+                .filter(appointment -> patientUuid.equals(appointment.getPatient().getUuid()))
+                .filter(appointment -> appointment.getStatus().equals(Scheduled))
+                .collect(Collectors.toList());
     }
 
 }
--- api/src/test/java/org/bahmni/module/bahmnimaadi/OpenmrsVisitAdviceTest.java
+++ api/src/test/java/org/bahmni/module/bahmnimaadi/OpenmrsVisitAdviceTest.java
@@ -1,15 +1,29 @@
 package org.bahmni.module.bahmnimaadi;
 
 import org.bahmni.module.bahmnimaadi.util.DateUtil;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.openmrs.Patient;
+import org.openmrs.Visit;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.appointments.model.Appointment;
 import org.openmrs.module.appointments.service.AppointmentsService;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.openmrs.module.appointments.model.AppointmentStatus.Cancelled;
+import static org.openmrs.module.appointments.model.AppointmentStatus.CheckedIn;
+import static org.openmrs.module.appointments.model.AppointmentStatus.Scheduled;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -21,20 +35,85 @@ public class OpenmrsVisitAdviceTest {
     @Mock
     private AppointmentsService appointmentsService;
 
-    @Test
-    public void shouldGetTodayAppointments() {
+    @Mock
+    private Patient patient;
+
+    @Mock
+    private Patient anotherPatient;
+
+    @Mock
+    private Visit visit;
+
+    @Mock
+    private Appointment appointment;
+
+    @Mock
+    private Appointment anotherAppointment;
+
+    @Mock
+    private Appointment otherAppointment;
+
+    private OpenmrsVisitAdvice visitAdvice;
+
+    @Before
+    public void setUp() {
         mockStatic(Context.class);
+        visitAdvice = new OpenmrsVisitAdvice();
+    }
+
+    @Test
+    public void shouldCheckInCurrentPatientsScheduledAppointments() throws NoSuchMethodException {
         mockStatic(DateUtil.class);
         when(Context.getService(AppointmentsService.class)).thenReturn(appointmentsService);
-        OpenmrsVisitAdvice visitAdvice = new OpenmrsVisitAdvice();
+        Date startOfDay = new Date();
+        when(DateUtil.startOfDay()).thenReturn(startOfDay);
+        when(appointmentsService.getAllAppointments(any())).thenReturn(Arrays.asList(appointment, anotherAppointment, otherAppointment));
+        String patientUuid = "patientUuid";
+        when(patient.getUuid()).thenReturn(patientUuid);
+        when(visit.getPatient()).thenReturn(patient);
+        when(appointment.getPatient()).thenReturn(patient);
+        when(appointment.getStatus()).thenReturn(Cancelled);
+        when(anotherAppointment.getPatient()).thenReturn(anotherPatient);
+        when(anotherAppointment.getStatus()).thenReturn(Scheduled);
+        when(otherAppointment.getPatient()).thenReturn(patient);
+        when(otherAppointment.getStatus()).thenReturn(Scheduled);
 
-        visitAdvice.afterReturning(null, null, null, null);
+        visitAdvice.afterReturning(null, this.getClass().getMethod("saveVisit"), new Object[]{visit}, null);
 
         verifyStatic();
         Context.getService(AppointmentsService.class);
         verifyStatic();
         DateUtil.startOfDay();
-        verify(appointmentsService).getAllAppointments(DateUtil.startOfDay());
+        verify(appointmentsService).getAllAppointments(startOfDay);
+        verify(visit).getPatient();
+        verify(patient, times(3)).getUuid();
+        verify(anotherPatient, times(1)).getUuid();
+        verify(appointment, times(1)).getPatient();
+        verify(appointment, times(1)).getStatus();
+        verify(anotherAppointment, times(1)).getPatient();
+        verify(anotherAppointment, never()).getStatus();
+        verify(otherAppointment, times(1)).getPatient();
+        verify(otherAppointment, times(1)).getStatus();
+        verify(appointmentsService, times(1))
+                .changeStatus(eq(otherAppointment), eq(CheckedIn.toString()), any(Date.class));
+        verify(appointmentsService, never())
+                .changeStatus(eq(appointment), eq(CheckedIn.toString()), any(Date.class));
+        verify(appointmentsService, never())
+                .changeStatus(eq(anotherAppointment), eq(CheckedIn.toString()), any(Date.class));
     }
 
+    @Test
+    public void shouldNotDoCheckInIfSaveVisitMethodNotCalled() throws NoSuchMethodException {
+
+        visitAdvice.afterReturning(null, this.getClass().getMethod("otherThanSave"), null, null);
+
+        verifyStatic(never());
+        Context.getService(AppointmentsService.class);
+    }
+
+    public void otherThanSave() {
+    }
+
+    public void saveVisit() {
+    }
 }
